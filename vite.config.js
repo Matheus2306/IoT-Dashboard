b147a5796ee92c47c34591ecb07491199705a326
matheus2306 <math2306.r@gmail.com>
2025-08-13 07:46:28 -0300

    feat: configure Vite server settings for external access and strict port availability

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:[{
+    host: true, // Permite acesso externo
+    port: 5000, // Porta do servidor de desenvolvimento
+    strictPort: true, // Garante que a porta especificada esteja disponível
+  }],
 })
